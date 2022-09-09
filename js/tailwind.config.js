3f94900f5e9a63bcb549452f208f37dd4e880ba4
abubakkerdev <abubakkerdev@gmail.com>
2022-09-09 21:08:59 +0600

    banner

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        "theme-black": "#282828",
+        "theme-yellow": "#FFB800",
+        "theme-headafter": "#5C6A92",
+        "theme-red": "#F40404",
+        "theme-overlay": "rgba(0,0,0,.6)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        banner: "url('./images/banner.webp')",
+      },
+    },
+  },
+};
