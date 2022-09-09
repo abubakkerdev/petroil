303c6e1db9a8b96ce6b5de6f039e3515c2c1e607
abubakkerdev <abubakkerdev@gmail.com>
2022-09-10 00:01:52 +0600

    service

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -8,12 +8,16 @@ tailwind.config = {
         "theme-headafter": "#5C6A92",
         "theme-red": "#F40404",
         "theme-overlay": "rgba(0,0,0,.6)",
+        "theme-content": "#6C6C6C",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
       backgroundImage: {
         banner: "url('./images/banner.webp')",
+        "service-1": "url('./images/service-1.webp')",
+        "service-2": "url('./images/service-2.webp')",
+        "service-3": "url('./images/service-3.webp')",
       },
     },
   },
